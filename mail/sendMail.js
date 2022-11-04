664719eac9a1a2c54954eaa98208fb198cb62d8d
gigo96 <sch102403@gmail.com>
2022-11-04 21:53:09 +0900

    mail 환경 변수의 명확한 구분을 위해 변수 재정의
    기존에 from 메일과 to 메일을 동시에 HOST로사용 중
    명확한 구분을 위해 환경 변수 분리 및 변수 명 재 정의
    
    메일 비밀번호는 기존 mail PASSWORD 환경 변수를 사용하여 어떠한 비밀번호인지
    명확히 구분이 어려운 문제 야기
    send를 위해 from 메일의 비밀번호를 제공하는 것이므로 기존
    PASSWORD를FROM_EMAIL_PASSWORD로 변경

--- mail/sendMail.js
+++ mail/sendMail.js
@@ -6,15 +6,15 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.HOST,
-    pass: process.env.PASSWORD,
+    user: process.env.FROM_EMAIL,
+    pass: process.env.FROM_EMAIL_PASSWORD,
   },
 });
 
 module.exports = async (mailBody) => {
   const mailOptions = {
-    from: process.env.HOST,
-    to: process.env.HOST,
+    from: process.env.FROM_EMAIL,
+    to: process.env.TO_EMAIL,
     subject: '오늘을 되돌아보시길 바랍니다.',
     html: mailBody,
   };
